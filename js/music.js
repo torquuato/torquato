3a858a5a6d947295f32e6274398fb1436ccda876
torquuato <markdimayuga06@gmail.com>
2025-11-16 01:22:42 +0000

    fixed music icon

--- js/music.js
+++ js/music.js
@@ -5,10 +5,10 @@ const button = document.getElementById("music-toggle");
 function toggleMusic() {
     if (isPlaying) {
         audio.pause();
-        button.src = "assets/music-off.png"; // change button image
+        button.src = "/torquato/assets/music-off.png"; // change button image
     } else {
         audio.play();
-        button.src = "assets/music-on.png"; // change button image
+        button.src = "/torquato/assets/music-on.png"; // change button image
     }
     isPlaying = !isPlaying;
 }
